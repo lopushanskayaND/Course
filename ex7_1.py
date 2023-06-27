87e20ae912fff2606aadee0c4aec6c6dfefd90a6
lopushanskayaND <137909608+lopushanskayaND@users.noreply.github.com>
2023-06-27 21:04:04 +0300

    Add files via upload

---
+++ ex7_1.py
@@ -0,0 +1,18 @@
+s = input()
+s1 = s[::-1]
+if s == s1:
+  print ('yes')
+else:
+  print ('no')
+
+    
+
+
+  
+
+  
+
+
+
+
+
---
+++ ex7_2.py
@@ -0,0 +1,20 @@
+s = input()
+s1 = s[0]
+for i in range (len(s)):
+  if s[i]!=' ':
+    s1 += s[i]
+  elif s1[-1] != ' ':
+    s1 += s[i]
+print (s1)
+
+    
+
+
+  
+
+  
+
+
+
+
+
