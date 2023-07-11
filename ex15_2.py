c7459dd38bafdf68bd5651d78522ccd0d470afbf
lopushanskayaND <137909608+lopushanskayaND@users.noreply.github.com>
2023-07-11 17:55:04 +0300

    Add files via upload

---
+++ ex15_2.py
@@ -0,0 +1,19 @@
+class Transport:
+  def __init__(self, name, max_speed, mileage):
+    self.name =  name
+    self.max_speed = max_speed
+    self.mileage = mileage
+class Autobus (Transport):
+  def __init__(self, capacity):
+    super().__init__(n, s, p)
+    self.capacity = 50
+
+n = input("Название автомобиля:")
+s  = int(input("Скорость:"))
+p = int (input("Пробег:"))
+Autobus1 = Autobus (35)
+print (f"Название автомобиля: {Autobus1.name} Скорость: {Autobus1.max_speed} Пробег: {Autobus1.mileage} Вместимость: {Autobus1.capacity}")
+       
+    
+  
+  
